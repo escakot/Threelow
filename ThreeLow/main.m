6d5c894f50e647cd343fb91c74f01d5ed7e00be3
escakot <ec@neorennie.com>
2017-06-28 16:46:48 -0400

    Initial Commit

---
+++ ThreeLow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeLow
+//
+//  Created by Errol Cheong on 2017-06-28.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
